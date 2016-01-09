eba4cbb6b2bd8dc928340317d677d09454f6b4bd
mookrs <mookrs@gmail.com>
2016-01-09 17:26:22 +0800

    Improve ListView

--- Chapter3/ListViewTest/app/src/main/java/com/example/listviewtest/FruitAdapter.java
+++ Chapter3/ListViewTest/app/src/main/java/com/example/listviewtest/FruitAdapter.java
@@ -26,11 +26,28 @@ public class FruitAdapter extends ArrayAdapter<Fruit> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         Fruit fruit = getItem(position);
-        View view = LayoutInflater.from(getContext()).inflate(resourceId, null);
-        ImageView fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
-        TextView fruitName = (TextView) view.findViewById(R.id.fruit_name);
-        fruitImage.setImageResource(fruit.getImageId());
-        fruitName.setText(fruit.getName());
+        View view;
+        ViewHolder viewHolder;
+        // 当convertView为空的时候，创建一个ViewHolder对象，
+        // 并将控件的实例都存放在ViewHolder里，然后调用View的setTag()方法，
+        // 将ViewHolder对象存储在View中
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(resourceId, null);
+            viewHolder = new ViewHolder();
+            viewHolder.fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            viewHolder.fruitName = (TextView) view.findViewById(R.id.fruit_name);
+            view.setTag(viewHolder);    // 将ViewHolder存储在View中
+        } else {
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();    // 重新获取ViewHolder
+        }
+        viewHolder.fruitImage.setImageResource(fruit.getImageId());
+        viewHolder.fruitName.setText(fruit.getName());
         return view;
+    }
+
+    class ViewHolder {
+        ImageView fruitImage;
+        TextView fruitName;
     }
 }
