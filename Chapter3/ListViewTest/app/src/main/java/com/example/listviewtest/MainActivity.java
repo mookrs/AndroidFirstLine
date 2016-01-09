46afbcaa2c278d75bcb93483ba07dd2134c723c2
mookrs <mookrs@gmail.com>
2016-01-09 17:31:34 +0800

    Set ListView items click event

--- Chapter3/ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
+++ Chapter3/ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,10 +23,17 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initFruits();
-        FruitAdapter adapter = new FruitAdapter(MainActivity.this,
-                R.layout.fruit_item, fruitList);
+        FruitAdapter adapter = new FruitAdapter(MainActivity.this, R.layout.fruit_item, fruitList);
         ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fruit fruit = fruitList.get(position);
+                Toast.makeText(MainActivity.this, fruit.getName(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void initFruits() {
