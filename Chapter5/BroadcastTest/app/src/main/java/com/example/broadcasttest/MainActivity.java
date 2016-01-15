52f78997f25800433756de6a2c3d57fb02db2234
mookrs <mookrs@gmail.com>
2016-01-15 12:02:15 +0800

    Check if network is available

--- Chapter5/BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.java
+++ Chapter5/BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -32,7 +34,13 @@ public class MainActivity extends AppCompatActivity {
     class NetworkChangeReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Toast.makeText(context, "network changes", Toast.LENGTH_SHORT).show();
+            ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isAvailable()) {
+                Toast.makeText(context, "network is available", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "network is unavailable", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
