e897974bc0e24fb160717c6e8b633f5229d1adbf
mookrs <mookrs@gmail.com>
2016-01-15 12:41:36 +0800

    Add MY_BROADCAST

--- Chapter5/BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.java
+++ Chapter5/BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.java
@@ -8,6 +8,8 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +25,15 @@ public class MainActivity extends AppCompatActivity {
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         networkChangeReceiver = new NetworkChangeReceiver();
         registerReceiver(networkChangeReceiver, intentFilter);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.broadcasttest.MY_BROADCAST");
+                sendBroadcast(intent);
+            }
+        });
     }
 
     @Override
---
+++ Chapter5/BroadcastTest/app/src/main/java/com/example/broadcasttest/MyBroadcastReceiver.java
@@ -0,0 +1,18 @@
+package com.example.broadcasttest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by mookrs on 16/1/15.
+ */
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "received in MyBroadcastReceiver", Toast.LENGTH_SHORT).show();
+    }
+
+}
