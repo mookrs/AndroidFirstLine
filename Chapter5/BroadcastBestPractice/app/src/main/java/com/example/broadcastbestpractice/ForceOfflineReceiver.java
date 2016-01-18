97b1c393ccc1c14da881f5a573442600010731c2
mookrs <mookrs@gmail.com>
2016-01-18 16:16:32 +0800

    Add comment about Android M

--- Chapter5/BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/ForceOfflineReceiver.java
+++ Chapter5/BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/ForceOfflineReceiver.java
@@ -31,6 +31,7 @@ public class ForceOfflineReceiver extends BroadcastReceiver {
         AlertDialog alertDialog = dialogBuilder.create();
         // 需要设置AlertDialog的类型，保证在广播接收器中可以正常弹出
         alertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+        // Android 6里可能因为权限的修改，这个对话框不能运行
         alertDialog.show();
     }
 }
