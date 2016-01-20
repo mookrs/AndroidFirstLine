4f5e4a1d04db04b2f5b801f2903e46d880c17450
mookrs <mookrs@gmail.com>
2016-01-20 21:09:26 +0800

    Restore data from SharedPreferences

--- Chapter6/SharedPreferencesTest/app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
+++ Chapter6/SharedPreferencesTest/app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
@@ -3,18 +3,21 @@ package com.example.sharedpreferencestest;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button saveData;
+    private Button restoreData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         saveData = (Button) findViewById(R.id.save_data);
+        restoreData = (Button) findViewById(R.id.restore_data);
         saveData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,6 +26,18 @@ public class MainActivity extends AppCompatActivity {
                 editor.putInt("age", 28);
                 editor.putBoolean("married", false);
                 editor.commit();
+            }
+        });
+        restoreData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
+                String name = pref.getString("name", "");
+                int age = pref.getInt("age", 0);
+                boolean married = pref.getBoolean("married", false);
+                Log.d("MainActivity", "name is " + name);
+                Log.d("MainActivity", "age is " + age);
+                Log.d("MainActivity", "married is " + married);
             }
         });
     }
