cf1d346049ec58fb34d03753d1b4fc7ec6f4cd7e
mookrs <mookrs@gmail.com>
2016-01-25 14:53:09 +0800

    Add CustomProvider

---
+++ Chapter7/DatabaseTest/app/src/main/java/com/example/databasetest/MyDatabaseHelper.java
@@ -0,0 +1,53 @@
+package com.example.databasetest;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+/**
+ * Created by mookrs on 16/1/21.
+ */
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String CREATE_BOOK = "create table book ("
+            + "id integer primary key autoincrement, "
+            + "author text, "
+            + "price real, "
+            + "pages integer, "
+            + "name text, "
+            + "category_id integer)";
+
+    public static final String CREATE_CATEGORY = "create table Category ("
+            + "id integer primary key autoincrement, "
+            + "category_name text, "
+            + "category_code integer)";
+
+    private Context mContext;
+
+    public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_BOOK);
+        db.execSQL(CREATE_CATEGORY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+//        db.execSQL("drop table if exists Book");
+//        db.execSQL("drop table if exists Category");
+//        onCreate(db);
+
+        switch (oldVersion) {
+            case 1:
+                db.execSQL(CREATE_CATEGORY);
+            case 2:
+                db.execSQL("alter table Book add column category_id integer");
+            default:
+        }
+    }
+}
